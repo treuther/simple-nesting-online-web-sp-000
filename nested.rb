ac896b61005aa96267378d517149a336c1580ab3
Tracy Reuther <tracyreuther@gmail.com>
2019-06-11 00:39:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto] = {
+	known_for: "Ruby",
+	languages: ["LISP", "C"]
+}
+
+programmer_hash
 end
 
 
